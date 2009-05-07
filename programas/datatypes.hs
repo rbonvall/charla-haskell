0e8576c24b6241e9aac243af5746bef7f17736f8
Roberto Bonvallet <rbonvall+github@gmail.com>
2009-05-07 17:15:05 -0400

    +datatype.hs, +tree.hs

---
+++ programas/datatypes.hs
@@ -0,0 +1,10 @@
+data Point = Point Float Float
+data Shape = Circle Point Float
+           | Rectangle Point Point
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2  
+surface (Rectangle (Point x1 y1)
+                   (Point x2 y2)) =
+       (abs $ x2 - x1) * (abs $ y2 - y1)  
+
