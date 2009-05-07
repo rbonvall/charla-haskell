987a56e61170018949c229e22f81de7ffd84c6e6
Roberto Bonvallet <rbonvall+github@gmail.com>
2009-05-07 16:46:52 -0400

    fold.cpp listo

--- programas/fold.cpp
+++ programas/fold.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <functional>
+#include <numeric>
+#include <vector>
 #include <string>
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y) )
@@ -7,47 +10,43 @@ using namespace std;
 
 float sum(float a[], int n) {
     int i;
-    /** begin sum **/
     float z = 0.0;
     for (i = 0; i < n; ++i)
         z = z + a[i];
-    /** end sum **/
     return z;
 }
 
 unsigned int max(unsigned int a[], int n) {
     int i;
-    /** begin max **/
     unsigned int z = 0;
     for (i = 0; i < n; ++i)
         z = MAX(z, a[i]);
-    /** end max **/
     return z;
 }
 
 bool any_even(int a[], int n) {
     int i;
-    /** begin anyeven **/
     bool z = false;
     for (i = 0; i < n; ++i)
         z = z || (a[i] % 2 == 0);
-    /** end anyeven **/
     return z;
 }
 
 string concat(string a[], int n) {
     int i;
-    /** begin concat **/
     string z("");
     for (i = 0; i < n; ++i)
         z.append(a[i]);
-    /** end concat **/
     return z;
 }
 
 
 int main() {
-    float sum_a = {4.3, 1.5, -7.0, 0.4};
-
+    float sum_a[] = {4.3, 1.5, -7.0, 0.4};
+    vector<float> a(&sum_a[0], &sum_a[5]);
+    float init = 0.0;
+    float sum_result = accumulate(a.begin(), a.end(),
+                                  init, plus<float>());
+    cout << sum_result << endl;
 }
 
